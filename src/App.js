1f172edd4beca629961003a30111863b33b30014
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-02-01 16:00:55 +0000

    reactjs website with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './components/About/About'
+import AllInOne from './components/AllInOne.jsx/AllinOne'
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
+import Pricing from './components/Pricing/Pricing'
+import Support from './components/Support/Support'
+import Footer from './components/Footer/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <About  />
+      <Support  />
+      <AllInOne />
+      <Pricing  />
+      <Footer   />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
